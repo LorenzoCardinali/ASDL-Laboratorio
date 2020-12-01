f68d28f1185e6b5828f7ba18706aa1f5290c54f9
LorenzoCardinali <74003643+LorenzoCardinali@users.noreply.github.com>
2020-12-01 14:39:23 +0100

    Aggiunta es9

---
+++ Esercitazione a Casa N9 - 27 novembre 2020/ASDL2021/src/it/unicam/cs/asdl2021/es9/CollisionListResizableHashTable.java
@@ -0,0 +1,294 @@
+/**
+ *
+ */
+package it.unicam.cs.asdl2021.es9;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Realizza un insieme tramite una tabella hash con indirizzamento primario (la
+ * funzione di hash primario deve essere passata come parametro nel costruttore
+ * e deve implementare l'interface PrimaryHashFunction) e liste di collisione.
+ *
+ * La tabella, poiché implementa l'interfaccia Set<E> non accetta elementi
+ * duplicati (individuati tramite il metodo equals() che si assume sia
+ * opportunamente ridefinito nella classe E) e non accetta elementi null.
+ *
+ * La tabella ha una dimensione iniziale di default (16) e un fattore di
+ * caricamento di defaut (0.75). Quando il fattore di bilanciamento effettivo
+ * eccede quello di default la tabella viene raddoppiata e viene fatto un
+ * riposizionamento di tutti gli elementi.
+ *
+ * @author Template: Luca Tesei, Implementazione: collettiva
+ *
+ */
+public class CollisionListResizableHashTable<E> implements Set<E> {
+
+    /*
+     * La capacità iniziale. E' una potenza di due e quindi la capacità sarà
+     * sempre una potenza di due, in quanto ogni resize raddoppia la tabella.
+     */
+    private static final int INITIAL_CAPACITY = 16;
+
+    /*
+     * Fattore di bilanciamento di default. Tipico valore.
+     */
+    private static final double LOAD_FACTOR = 0.75;
+
+    /*
+     * Numero di elementi effettivamente presenti nella hash table in questo
+     * momento. ATTENZIONE: questo valore è diverso dalla capacity, che è la
+     * lunghezza attuale dell'array di Object che rappresenta la tabella.
+     */
+    private int size;
+
+    /*
+     * L'idea è che l'elemento in posizione i della tabella hash è un bucket che
+     * contiene null oppure il puntatore al primo nodo di una lista concatenata
+     * di elementi. Si può riprendere e adattare il proprio codice della
+     * Esercitazione 6 che realizzava una lista concatenata di elementi
+     * generici. La classe interna Node<E> è ripresa proprio da lì.
+     *
+     * ATTENZIONE: la tabella hash vera e propria può essere solo un generico
+     * array di Object e non di Node<E> per una impossibilità del compilatore di
+     * accettare di creare array a runtime con un tipo generics. Ciò infatti
+     * comporterebbe dei problemi nel sistema di check dei tipi Java che, a
+     * run-time, potrebbe eseguire degli assegnamenti in violazione del tipo
+     * effettivo della variabile. Quindi usiamo un array di Object che
+     * riempiremo sempre con null o con puntatori a oggetti di tipo Node<E>.
+     *
+     * Per inserire un elemento nella tabella possiamo usare il polimorfismo di
+     * Object:
+     *
+     * this.table[i] = new Node<E>(item, next);
+     *
+     * ma quando dobbiamo prendere un elemento dalla tabella saremo costretti a
+     * fare un cast esplicito:
+     *
+     * Node<E> myNode = (Node<E>) this.table[i];
+     *
+     * Ci sarà dato un warning di cast non controllato, ma possiamo eliminarlo
+     * con un tag @SuppressWarning,
+     */
+    private Object[] table;
+
+    /*
+     * Funzion di hash primaria usata da questa hash table. Va inizializzata nel
+     * costruttore all'atto di creazione dell'oggetto.
+     */
+    private final PrimaryHashFunction phf;
+
+    /*
+     * Contatore del numero di modifiche. Serve per rendere l'iterator
+     * fail-fast.
+     */
+    private int modCount;
+
+    // I due metodi seguenti sono di comodo per gestire la capacity e la soglia
+    // oltre la quale bisogna fare il resize.
+
+    /* Numero di elementi della tabella corrente */
+    private int getCurrentCapacity() {
+        return this.table.length;
+    }
+
+    ;
+
+    /*
+     * Valore corrente soglia oltre la quale si deve fare la resize,
+     * getCurrentCapacity * LOAD_FACTOR
+     */
+    private int getCurrentThreshold() {
+        return (int) (getCurrentCapacity() * LOAD_FACTOR);
+    }
+
+    /**
+     * Costruisce una Hash Table con capacità iniziale di default e fattore di
+     * caricamento di default.
+     */
+    public CollisionListResizableHashTable(PrimaryHashFunction phf) {
+        this.phf = phf;
+        this.table = new Object[INITIAL_CAPACITY];
+        this.size = 0;
+        this.modCount = 0;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        // TODO implementare
+        /*
+         * ATTENZIONE: usare l'hashCode dell'oggetto e la funzione di hash
+         * primaria passata all'atto della creazione: il bucket in cui cercare
+         * l'oggetto o è la posizione
+         * this.phf.hash(o.hashCode(),this.getCurrentCapacity)
+         *
+         * In questa posizione, se non vuota, si deve cercare l'elemento o
+         * utilizzando il metodo equals() su tutti gli elementi della lista
+         * concatenata lì presente
+         *
+         */
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Itr();
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException("Operazione non supportata");
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        throw new UnsupportedOperationException("Operazione non supportata");
+    }
+
+    @Override
+    public boolean add(E e) {
+        // TODO implementare
+        /*
+         * ATTENZIONE: usare l'hashCode dell'oggetto e la funzione di hash
+         * primaria passata all'atto della creazione: il bucket in cui inserire
+         * l'oggetto o è la posizione
+         * this.phf.hash(o.hashCode(),this.getCurrentCapacity)
+         *
+         * In questa posizione, se non vuota, si deve inserire l'elemento o
+         * nella lista concatenata lì presente. Se vuota, si crea la lista
+         * concatenata e si inserisce l'elemento, che sarà l'unico.
+         *
+         */
+        // ATTENZIONE, si inserisca prima il nuovo elemento e poi si controlli
+        // se bisogna fare resize(), cioè se this.size >
+        // this.getCurrentThreshold()
+        return false;
+    }
+
+    /*
+     * Raddoppia la tabella corrente e riposiziona tutti gli elementi. Da
+     * chiamare quando this.size diventa maggiore di getCurrentThreshold()
+     */
+    private void resize() {
+        // TODO implementare
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        // TODO implementare
+        /*
+         * ATTENZIONE: usare l'hashCode dell'oggetto e la funzione di hash
+         * primaria passata all'atto della creazione: il bucket in cui cercare
+         * l'oggetto o è la posizione
+         * this.phf.hash(o.hashCode(),this.getCurrentCapacity)
+         *
+         * In questa posizione, se non vuota, si deve cercare l'elemento o
+         * utilizzando il metodo equals() su tutti gli elementi della lista
+         * concatenata lì presente. Se presente, l'elemento deve essere
+         * eliminato dalla lista concatenata
+         *
+         */
+        // ATTENZIONE: la rimozione, in questa implementazione, **non** comporta
+        // mai una resize "al ribasso", cioè un dimezzamento della tabella se si
+        // scende sotto il fattore di bilanciamento desiderato.
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        // TODO implementare
+        // utilizzare un iteratore della collection e chiamare il metodo
+        // contains
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        // TODO implementare
+        // utilizzare un iteratore della collection e chiamare il metodo add
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Operazione non supportata");
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        // TODO implementare
+        // utilizzare un iteratore della collection e chiamare il metodo remove
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        // Ritorno alla situazione iniziale
+        this.table = new Object[INITIAL_CAPACITY];
+        this.size = 0;
+        this.modCount = 0;
+    }
+
+    /*
+     * Classe per i nodi della lista concatenata
+     */
+    private static class Node<E> {
+        private E item;
+
+        private Node<E> next;
+
+        /*
+         * Crea un nodo "singolo" equivalente a una lista con un solo elemento.
+         */
+        Node(E item, Node<E> next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    /*
+     * Classe che realizza un iteratore per questa hash table. L'ordine in cui
+     * vengono restituiti gli oggetti presenti non è rilevante, ma ogni oggetto
+     * presente deve essere restituito dall'iteratore una e una sola volta.
+     * L'iteratore deve essere fail-fast, cioè deve lanciare una eccezione
+     * IllegalStateException se a una chiamata di next() si "accorge" che la
+     * tabella è stata cambiata rispetto a quando l'iteratore è stato creato.
+     */
+    private class Itr implements Iterator<E> {
+
+        // TODO inserire le variabili che servono
+
+        private int numeroModificheAtteso;
+
+        private Itr() {
+            // TODO implementare il resto
+            this.numeroModificheAtteso = modCount;
+        }
+
+        @Override
+        public boolean hasNext() {
+            // TODO implementare
+            return false;
+        }
+
+        @Override
+        public E next() {
+            // TODO implementare
+            return null;
+        }
+
+    }
+
+}
